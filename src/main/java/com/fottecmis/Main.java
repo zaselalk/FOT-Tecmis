940d5d151f0e0d7dafc21a5e85651ef39eb38644
Asela <43272744+zaselalk@users.noreply.github.com>
2024-04-18 11:02:51 +0530

    minor change

--- src/main/java/com/fottecmis/Main.java
+++ src/main/java/com/fottecmis/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Helloa!");
         stage.setScene(scene);
         stage.show();
     }
