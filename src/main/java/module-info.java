93ea54006e91354709d4c2423a74bc5fc1272aaf
Asela <43272744+zaselalk@users.noreply.github.com>
2024-04-18 12:36:14 +0530

    mysql connection comment

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.fottecmis.fottecmis {
     requires javafx.controls;
     requires javafx.fxml;
+//    requires mysql.connector.java;
+    requires java.sql;
 
 
     opens com.fottecmis to javafx.fxml;
