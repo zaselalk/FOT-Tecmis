b21eeee59733762e2f34010afec519a28b634d49
Asela <43272744+zaselalk@users.noreply.github.com>
2024-04-18 13:05:07 +0530

    database connection class updated

--- src/main/java/com/fottecmis/Database/GetConnection.java
+++ src/main/java/com/fottecmis/Database/GetConnection.java
@@ -2,26 +2,30 @@ package com.fottecmis.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class GetConnection {
+    private static final Logger LOGGER = Logger.getLogger(GetConnection.class.getName());
     private void registerConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Successfully registerd");
-        } catch (Exception e) {
-            System.out.println(e);
+        } catch (ClassNotFoundException e) {
+            LOGGER.log(Level.SEVERE, "Error in registering the driver", e);
         }
     }
 
-    public Connection getMyConnection(String user, String password) {
+    public Connection getMyConnection(String user, String password) throws DatabaseConnectionException {
         try {
             registerConnection();
             String url = "Jdbc:mysql://localhost:3306/student";
 
             return DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Error in getting connection", e);
+            throw new DatabaseConnectionException("Error in getting connection");
         }
     }
 }
