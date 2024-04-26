821276a8680c7ee6b913b7a85f064278bce05502
Asela <43272744+zaselalk@users.noreply.github.com>
2024-04-26 23:12:33 +0530

    Update navigation on student dashboard

--- src/main/java/com/fottecmis/Student/StudentController.java
+++ src/main/java/com/fottecmis/Student/StudentController.java
@@ -1,4 +1,43 @@
 package com.fottecmis.Student;
 
+import com.fottecmis.Shared.SceneHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
 public class StudentController {
+    public void viewStudentCourse(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student-course");
+        Parent studentCourseScene = loader.load();
+        SceneHandler.switchScene(event, studentCourseScene);
+
+    }
+
+    public void viewStudentAttendance(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student-attendance");
+        Parent studentAttendanceScene = loader.load();
+        SceneHandler.switchScene(event, studentAttendanceScene);
+
+    }
+
+    public void viewStudentGrade(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student-grade");
+        Parent studentGradeScene = loader.load();
+        SceneHandler.switchScene(event, studentGradeScene);
+
+    }
+
+    public void viewStudentTimetable(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student-timetable");
+        Parent studentTimetableScene = loader.load();
+        SceneHandler.switchScene(event, studentTimetableScene);
+    }
+
+    public void viewStudentMedical(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student-medical");
+        Parent studentMedicalScene = loader.load();
+        SceneHandler.switchScene(event, studentMedicalScene);
+    }
 }
---
+++ src/main/java/com/fottecmis/Student/StudentDashboard.java
@@ -0,0 +1,18 @@
+package com.fottecmis.Student;
+
+import com.fottecmis.Shared.SceneHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class StudentDashboard {
+    public void showStudentDashboard(ActionEvent event) throws IOException {
+        FXMLLoader loader = SceneHandler.createLoader("Student/student");
+        Parent studentDashboardScene = loader.load();
+        SceneHandler.switchScene(event, studentDashboardScene);
+    }
+
+
+}
