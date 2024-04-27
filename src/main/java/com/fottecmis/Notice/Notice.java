e379964efb03d3fc0a9de5b1608186dc0c64f9d3
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2024-04-28 00:00:34 +0530

    Notice.java file created

--- src/main/java/com/fottecmis/Notice/Notice.java
+++ src/main/java/com/fottecmis/Notice/Notice.java
@@ -1,5 +1,65 @@
 package com.fottecmis.Notice;
 
+import java.util.Date;
+
+public class Notice {
+    public int noticeId;
+    private Date date;
+    private String title;
+    private String description;
+    private String filepath;
+
+
+    public int getNoticeId(){
+        return noticeId;
+    }
+    public void setNoticeId(int noticeId){
+        this.noticeId=noticeId;
+    }
+
+    public Date getDate(){
+        return date;
+    }
+    public void setDate(Date date){
+        this.date=date;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+
+    public String getFilepath(){
+        return filepath;
+    }
+    public void setFilepath(String filepath){
+        this.filepath=filepath;
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.fottecmis.Notice;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
@@ -21,3 +81,4 @@ public class Notice {
     }
 
 }
+*/
